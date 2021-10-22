df37560f01dc408ad42a863c2c3952f67b6a1c73
jhrdt <jhrdt@users.noreply.github.com>
2021-10-22 11:14:29 +0200

    Remove duplicate line

--- admin_locking/static/admin_locking/admin_locking.js
+++ admin_locking/static/admin_locking/admin_locking.js
@@ -10,7 +10,6 @@
     update_lock();
     // clear lock when clicking "save"
     $('div.submit-row input[type=submit]').click(clear_lock);
-    $(window).unload(clear_lock);
     try {
       $(window).unload(clear_lock);
     } catch (e) {
