0eb8d0d553fbf50e020e76cb6693a0620a33ada9
jhrdt <jhrdt@users.noreply.github.com>
2021-10-27 09:22:35 +0200

    Fix issue with not loaded django.jQuery
    Ensures that django.jQuery is ready before the js runs.

--- admin_locking/static/admin_locking/admin_locking.js
+++ admin_locking/static/admin_locking/admin_locking.js
@@ -1,74 +1,76 @@
-(function($) {
-  var lock_url = window.location.pathname + 'locking/',
-      csrftoken = getCookie('csrftoken'), timer;
+window.addEventListener("load", function() {
+  (function($) {
+    var lock_url = window.location.pathname + 'locking/',
+        csrftoken = getCookie('csrftoken'), timer;
 
-  $(function() {
-    // only applies to edit forms
-    if (!$('body').hasClass('change-form') || (window.location.pathname.indexOf('/add/') > -1)){return;}
+    $(function() {
+      // only applies to edit forms
+      if (!$('body').hasClass('change-form') || (window.location.pathname.indexOf('/add/') > -1)){return;}
 
-    // check and set lock
-    update_lock();
-    // clear lock when clicking "save"
-    $('div.submit-row input[type=submit]').click(clear_lock);
-    try {
-      $(window).unload(clear_lock);
-    } catch (e) {
-      $(window).on('unload', function(){
-        clear_lock;
-      });
-    }
-  });
-
-  function update_lock() {
-    $.ajax({
-      url: lock_url,
-      type: 'post',
-      data: {url: window.location.pathname},
-      headers: {"X-CSRFToken": csrftoken},
-      dataType: 'json',
-      success: function(data) {
-        if ((data.status === 'locking') && $('div.submit-row:first').hasClass('locked'))  {
-          // we just acquired the lock while it was locked before
-          $('div.submit-row').html('<p>' + data.message + ' <a href="' +
-              window.location.pathname + '">' + data.link_message + '</a></p>')
-            .removeClass('locked').addClass('unlocked');
-        }
-        if ((data.status === 'locked') && !$('div.submit-row:first').hasClass('locked')) {
-          // it's still locked
-          $('div.submit-row').html('<p>' + data.message + '</p>').addClass('locked').css({background: '#d00', color: '#fff', textAlign: 'center'});
-          $('#content-main input,textarea,select').prop('readonly', true).prop('disabled', true);
-        }
-        // keep checking/setting lock while on the page
-        timer = setTimeout(update_lock, data.lock_timeout*1000);
+      // check and set lock
+      update_lock();
+      // clear lock when clicking "save"
+      $('div.submit-row input[type=submit]').click(clear_lock);
+      try {
+        $(window).unload(clear_lock);
+      } catch (e) {
+        $(window).on('unload', function(){
+          clear_lock;
+        });
       }
     });
-  }
 
-  function clear_lock() {
-    if (!$('div.submit-row').hasClass('locked')) {
+    function update_lock() {
       $.ajax({
         url: lock_url,
         type: 'post',
-        data: {url: window.location.pathname, action: 'clear'},
+        data: {url: window.location.pathname},
         headers: {"X-CSRFToken": csrftoken},
         dataType: 'json',
+        success: function(data) {
+          if ((data.status === 'locking') && $('div.submit-row:first').hasClass('locked'))  {
+            // we just acquired the lock while it was locked before
+            $('div.submit-row').html('<p>' + data.message + ' <a href="' +
+                window.location.pathname + '">' + data.link_message + '</a></p>')
+              .removeClass('locked').addClass('unlocked');
+          }
+          if ((data.status === 'locked') && !$('div.submit-row:first').hasClass('locked')) {
+            // it's still locked
+            $('div.submit-row').html('<p>' + data.message + '</p>').addClass('locked').css({background: '#d00', color: '#fff', textAlign: 'center'});
+            $('#content-main input,textarea,select').prop('readonly', true).prop('disabled', true);
+          }
+          // keep checking/setting lock while on the page
+          timer = setTimeout(update_lock, data.lock_timeout*1000);
+        }
       });
     }
-  }
 
-  function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = $.trim(cookies[i]);
-        // Does this cookie string begin with the name we want?
-        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-          break;
-        }
+    function clear_lock() {
+      if (!$('div.submit-row').hasClass('locked')) {
+        $.ajax({
+          url: lock_url,
+          type: 'post',
+          data: {url: window.location.pathname, action: 'clear'},
+          headers: {"X-CSRFToken": csrftoken},
+          dataType: 'json',
+        });
       }
     }
-    return cookieValue;
-  }
-})(django.jQuery);
+
+    function getCookie(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = $.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+      return cookieValue;
+    }
+  })(django.jQuery)
+});
